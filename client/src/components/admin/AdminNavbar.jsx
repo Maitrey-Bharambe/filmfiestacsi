1676ea665b68eb3119ce702410b116a5534d0516
madhav <madhavpalav100@gmail.com>
2025-09-19 11:39:19 +0530

    test

--- client/src/components/admin/AdminNavbar.jsx
+++ client/src/components/admin/AdminNavbar.jsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
-import { assets } from "../../assets/assets";
+import film_fiesta_logo from "../assets/film_fiesta_logo.png";
 
 const AdminNavbar = () => {
   return (
     <div className="flex items-center justify-between px-6 md:px-10 h-16 border-b border-gray-300/30">
       <Link to="/">
-        <img src={assets.logo} alt="logo" className="w-36 h-auto" />
+        <img src={film_fiesta_logo} alt="logo" className="w-36 h-auto" />
       </Link>
     </div>
   );
